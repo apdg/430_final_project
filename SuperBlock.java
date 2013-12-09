4fdbc05b964941e57c8eac9779c6c314757804a0
Chris Burgess <chrisb@cetoncorp.com>
2013-12-08 20:15:20 -0800

    Wrote SuperBlock.java, not yet tested

--- SuperBlock.java
+++ SuperBlock.java
@@ -7,6 +7,7 @@ public class SuperBlock {
 	public int inodeBlocks;
 	public int freeList;
 
+
 	public SuperBlock( int diskSize ) {
 		byte[] superBlock = new byte[Disk.blockSize];
 		SysLib.rawread( 0, superBlock );
@@ -29,26 +30,45 @@ public class SuperBlock {
 	void sync(){
 		// **TODO** 
 		// write totalBlocks, inodeBlocks, freelist to disk
+        byte[] superBlock = new byte[Disk.blockSize];
+        int offset = 0;
+        SysLib.int2bytes(totalBlocks, superBlock, offset);
+        SysLib.int2bytes(inodeBlocks, superBlock, offset + 4);
+        SysLib.int2bytes(freeList, superBlock, offset + 8);
+        SysLib.rawwrite(0, superBlock);
 	}
 
 	void format(){
 		// **TODO**
 		// initialize Inodes, free blocks
-	}
+        inodeBlocks = defaultInodeBlocks;
+        freeBlocks = totalBlocks - inodeBlocks - 2; // superblock and directoryblock
+    }
 	
 	void format( int numBlocks ){
 		// **TODO**
 		// initialize Inodes, free blocks
+        inodeBlocks = numBlocks;
+        freeBlocks = totalBlocks - inodeBlocks - 2; // superblock and directoryblock
 	}
 
 	public int getFreeBlock(){
 		// **TODO**
 		// dequeue top block in freelist
+        int free = freeBlocks;
+        byte[] data = SysLib.rawread(free, freeBlocks);
+        short next = bytes2short(data, 0);
+        freeBlocks = next;
+        return free;
 	}
 
 	public boolean returnBlock( int oldBlockNumber ){
 		// **TODO**
 		// enqueue oldBlockNumber to top of freelist
+        byte[] freeBlock = new byte[Disk.blockSize];
+        int next = freeBlocks;
+        SysLib.int2bytes(next, freeBlock, 0);
+        SysLib.rawwrite(oldBlockNumber, freeBlock);
+        freeBlocks = oldBlockNumber;
 	}
-
 }
