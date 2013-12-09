20173e6fe599466c53a4351f416a95102938503e
Chris Burgess <chrisb@cetoncorp.com>
2013-12-08 18:47:05 -0800

    Debugged Directory.java, should be good to go

--- Directory.java
+++ Directory.java
@@ -9,27 +9,27 @@ public class Directory {
 	private char fnames[][];    // each element stores a different file name.
 
 	public Directory( int maxInumber ) { // directory constructor
-		fsizes = new int[maxInumber];     // maxInumber = max files
+	fsize = new int[maxInumber];     // maxInumber = max files
 		for ( int i = 0; i < maxInumber; i++ ) 
 			fsize[i] = 0;                 // all file size initialized to 0
 		fnames = new char[maxInumber][maxChars];
 		String root = "/";                // entry(inode) 0 is "/"
 		fsize[0] = root.length( );        // fsize[0] is the size of "/".
-		root.getChars( 0, fsizes[0], fnames[0], 0 ); // fnames[0] includes "/"
+		root.getChars( 0, fsize[0], fnames[0], 0 ); // fnames[0] includes "/"
 	}
 
 	// assumes data[] received directory information from disk
 	// initializes the Directory instance with this data[]
 	public int bytes2directory( byte data[] ) {
 		int offset = 0;
-		for ( int i = 0; i < fsizes.length; i++, offset += 4 ) {
-			fsizes[i] = SysLib.bytes2int( data, offset );
+		for ( int i = 0; i < fsize.length; i++, offset += 4 ) {
+			fsize[i] = SysLib.bytes2int( data, offset );
         } 
         for ( int i = 0; i < fnames.length; i++, offset += maxChars * 2 ) {
 			String fname = new String( data, offset, maxChars * 2 );
-			fname.getChars( 0, fsizes[i], fnames[i], 0 );
+			fname.getChars( 0, fsize[i], fnames[i], 0 );
 		}
-
+        return 0;
 	}
 
     // Converts the Directory information into a plain byte array and returns it.
@@ -37,55 +37,95 @@ public class Directory {
 	public byte[] directory2bytes( ) {
         int byte_array_size = fsize.length * 4 +
                               fnames.length * maxChars * 2;
-        byte data[byte_array_size];
+        byte data[] = new byte[byte_array_size];
         int offset = 0;
         // add fsize
-        for (int i = 0; i < fsizes.length; i++, offset += 4) {
-            SysLib.int2bytes(fsizes[i], data, offset);
+        for (int i = 0; i < fsize.length; i++, offset += 4) {
+            SysLib.int2bytes(fsize[i], data, offset);
         }
         // add fnames
-        for (int i = 0; i < fnames.length, i++, offset += maxChars * 2) {
-            for (int j = 0; j < fsizes[i]; j++) {
-                short2bytes((short)fnames[i][j], data, offset + j);
+        for (int i = 0; i < fnames.length; i++, offset += maxChars * 2) {
+            for (int j = 0; j < fsize[i]; j++) {
+                SysLib.short2bytes((short)fnames[i][j], data, offset + j*2);
             }
         }
         return data;
 	}
 
-    
-	public short ialloc( String filename ) {
+   
+
+
 	// filename is the one of a file to be created.
 	// allocates a new inode number for this filename
+	public short ialloc( String filename ) {
         short iNumber = 0;
-        for (int i = 0; i < fsizes.length; i++) {
-            if (fsizes[i] == 0) {
-                iNumber = i;
+        for (int i = 0; i < fsize.length; i++) {
+            if (fsize[i] == 0) {
+                iNumber = (short)i;
                 break;
             }
         }
 
-        fsizes[iNumber] = filename.size();
-        filename.getChars(0, fsizes[iNumber], fnames[iNumber], 0);
+        fsize[iNumber] = filename.length();
 
-        return num_inodes;
+        filename.getChars(0, fsize[iNumber], fnames[iNumber], 0);
+        
+        return iNumber;
 	}
 
-	public boolean ifree( short iNumber ) {
+
+
 	// deallocates this inumber (inode number)
 	// the corresponding file will be deleted.
-        fsizes[iNumber] = 0;
+	public boolean ifree( short iNumber ) {
+        for (int i = 0; i < fsize[iNumber]; i++){
+            fnames[iNumber][i] = (char)0;
+        }
+        fsize[iNumber] = 0;
         return true;
 	}
 
-	public short namei( String filename ) {
+
+
 	// returns the inumber corresponding to this filename
-	    short iNumber;
+	public short namei( String filename ) {
+	    short iNumber = -1;
         for (int i = 0; i < fnames.length; i++, iNumber++) {
-            String compname = new String(fnames[i], 0, fsizes[i]);
+            String compname = new String(fnames[i], 0, fsize[i]);
             if (compname.compareTo(filename) == 0) {
-                return iNumber;
+                iNumber = (short)i;
+                break;
             }
         }
+        return iNumber;
     }
 
+
+    /* FOR TESTING PURPOSES ONLY
+    public void short2bytes(short s, byte[] b, int offset) {
+        b[offset] = (byte)(s >> 8);
+        b[offset + 1] = (byte)s;
+    }
+
+    public short bytes2short(byte[] b, int offset) {
+        short s = 0;
+        s += b[offset] & 0xff;
+        s <<= 8;
+        s += b[offset + 1] & 0xff;
+        return s;
+    }
+
+    public void int2bytes(int i, byte[] b, int offset) {
+        b[offset] = (byte)(i >> 24);
+        b[offset + 1] = (byte)(i >> 16);
+        b[offset + 2] = (byte)(i >> 8);
+        b[offset + 3] = (byte)i;
+    }
+
+    public int bytes2int(byte[] b, int offset) {
+        int n = ((b[offset] & 0xff) << 24) + ((b[offset+1] & 0xff) << 16) +
+                ((b[offset+2] & 0xff) << 8) + (b[offset+3] & 0xff);
+        return n;
+    }
+    */
 }
